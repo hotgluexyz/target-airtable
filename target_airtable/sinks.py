fe3a47e7a2bac0cfb7c47280b9be5c3fe9f4a4db
Hassan Syyid <hassansyyid@gmail.com>
2021-07-08 18:47:27 -0400

    Initial commit

---
+++ target_airtable/sinks.py
@@ -0,0 +1,52 @@
+"""Airtable target stream class, which handles writing streams."""
+
+from typing import Any, Dict, List, Tuple, Union
+
+
+from singer_sdk.sinks import BatchSink
+
+import requests
+import urllib.parse
+
+class AirtableSink(BatchSink):
+    """Airtable target sink class."""
+
+    max_size = 10
+
+    def process_batch(self, context: dict) -> None:
+        """Write out any prepped records and return once fully written."""
+        # The SDK populates `context["records"]` automatically
+        # since we do not override `process_record()`.
+
+        def preprocess_records(x):
+            # Wrap every record in a JSON object under the fields
+            return {
+                "fields": x
+            }
+
+        records = [preprocess_records(x) for x in context["records"]]
+        # Get the records_url (we have a default, but that may change)
+        records_url = self.config.get("records_url", "https://api.airtable.com/v0")
+        # Get the base id
+        base_id = self.config.get("base_id")
+        # Get the table name (URL encoded)
+        table_name = urllib.parse.quote(self.config.get("table_name"))
+        token = self.config.get("token")
+        endpoint = f"{records_url}/{base_id}/{table_name}"
+
+        # Make the request
+        r = requests.post(endpoint, headers={
+            "Authorization": f"Bearer {token}"
+        },json={
+            "records": records,
+            "typecast": True # allow typecasting to fit Airtable schema
+        })
+
+        self.logger.info(f"Uploaded {len(records)} | success={r.ok}")
+
+        if not r.ok:
+            # If request fails, log error message
+            self.logger.error(r.text)
+
+        # Clean up records
+        context["records"] = []
---
+++ target_airtable/target.py
@@ -0,0 +1,25 @@
+"""Airtable target class."""
+
+from pathlib import Path
+from typing import List
+
+from singer_sdk.target_base import Target
+from singer_sdk.sinks import Sink
+from singer_sdk import typing as th
+
+from target_airtable.sinks import (
+    AirtableSink,
+)
+
+
+class TargetAirtable(Target):
+    """Sample target for Airtable."""
+
+    name = "target-airtable"
+    config_jsonschema = th.PropertiesList(
+        th.Property("token", th.StringType, required=True),
+        th.Property("base_id", th.StringType, required=True),
+        th.Property("table_name", th.StringType, required=True),
+        th.Property("records_url", th.StringType)
+    ).to_dict()
+    default_sink_class = AirtableSink
